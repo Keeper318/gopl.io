081a7a88df4614061dd9f0203bb29dabbdbd2895
Filipp Zapolskikh <fmzapolskikh@edu.hse.ru>
2026-09-02 19:28:58 +0400

    Exercise 1.4

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -1,5 +1,6 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Modified by Filipp Zapolskikh
 
 // See page 10.
 //!+
@@ -17,8 +18,10 @@ import (
 func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
+	file_lines := make(map[*os.File]map[string]bool)
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		file_lines[os.Stdin] = make(map[string]bool)
+		countLines(os.Stdin, counts, file_lines[os.Stdin])
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,21 +29,29 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			file_lines[f] = make(map[string]bool)
+			countLines(f, counts, file_lines[f])
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			var files_with_line []string
+			for file := range file_lines {
+				if file_lines[file][line] {
+					files_with_line = append(files_with_line, file.Name())
+				}
+			}
+			fmt.Printf("%d\t%v\t%s\n", n, files_with_line, line)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, lines map[string]bool) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		lines[input.Text()] = true
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
