15e4fd5234d5b8507fd65e2f7f4950e25bf53041
Filipp Zapolskikh <fmzapolskikh@edu.hse.ru>
2026-09-03 19:29:03 +0400

    Exercise 1.8

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -13,10 +13,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.Contains(url, "://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
