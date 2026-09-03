5aa946a426bdf59fbd16d6d28692f7cf4707076c
Filipp Zapolskikh <fmzapolskikh@edu.hse.ru>
2026-09-03 19:20:29 +0400

    Exercise 1.7

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -1,5 +1,6 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Modified by Filipp Zapolskikh
 
 // See page 16.
 //!+
@@ -9,7 +10,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -21,13 +22,12 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
 
