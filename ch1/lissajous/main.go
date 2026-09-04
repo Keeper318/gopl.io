b92b8b2bdb16feb0a8fdb49e53639e7e05c9194c
Filipp Zapolskikh <fmzapolskikh@edu.hse.ru>
2026-09-04 19:05:37 +0400

    Exercise 1.12

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -24,6 +24,8 @@ import (
 import (
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -47,7 +49,10 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "web" {
 		//!+http
 		handler := func(w http.ResponseWriter, r *http.Request) {
-			lissajous(w)
+			if err := r.ParseForm(); err != nil {
+				log.Print(err)
+			}
+			lissajous(w, r.Form)
 		}
 		http.HandleFunc("/", handler)
 		//!-http
@@ -55,16 +60,25 @@ func main() {
 		return
 	}
 	//!+main
-	lissajous(os.Stdout)
+	lissajous(os.Stdout, nil)
 }
 
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
+func intParam(form url.Values, name string, defaultValue int) int {
+	if value, ok := form[name]; ok {
+		if num, err := strconv.Atoi(value[0]); err == nil {
+			return num
+		}
+	}
+	return defaultValue
+}
+
+func lissajous(out io.Writer, form url.Values) {
+	var (
+		cycles  = intParam(form, "cycles", 5)   // number of complete x oscillator revolutions
+		res = 0.001                                             // angular resolution
+		size    = intParam(form, "size", 100)   // image canvas covers [-size..+size]
+		nframes = intParam(form, "nframes", 64) // number of animation frames
+		delay   = intParam(form, "delay", 8)    // delay between frames in 10ms units
 	)
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
@@ -76,10 +90,10 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette[:])
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5),
 				uint8(1+i%shades))
 		}
 		phase += 0.1
